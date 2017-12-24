fa5bc1728f451b9f43deaaa0378517945bb7f338
danielmichaels <dans.address@outlook.com>
2017-12-24 19:27:59 +0100

    beginning

---
+++ ls_clone.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import os
+import pathlib
+
+""" What does 'ls' do?
+BASIC: lists directory --> it needs a start point --> prints file names
+ADV: prints, rwx, size, last edited, owner, group
+
+"""
+
+def main():
+    banner()
+    pass
+
+def cli():
+    """ Where user inputs ls commands """
+    pass
+
+def printer():
+    pass
+
+def banner():
+    print()
+    print('#'*40)
+    print('\t\tList Directory Tool')
+    print('#'*40)
+
+if __name__ == '__main__':
+    main()
