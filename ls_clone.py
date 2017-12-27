dd9c27830c89f5eef1bc4a6635d4a192adf2f1bc
danielmichaels <dans.address@outlook.com>
2017-12-27 23:04:58 +0100

    Rewrote script to be a class based.

--- ls_clone.py
+++ ls_clone.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import datetime
 import os
+import pwd
 
 """ What does 'ls' do?
 A very basic implementation of the unix 'ls' tool.
@@ -14,7 +15,8 @@ ADV: prints, rwx, size, last edited, owner, group
 def main():
     banner()
     command = cli()
-    printer(command)
+    list_directory = ListStructure()
+    printer(command, list_directory)
 
 
 def cli():
@@ -23,61 +25,64 @@ def cli():
     return command
 
 
-def printer(command):
+def printer(command, list_directory):
     """ Return the ls command"""
     # ls = 'ls'
     # ls_l = 'ls -l'
     # ls_la = 'ls -la'
 
     if command == 'ls':
-        ls()
+        print(list_directory.ls())
 
     elif command == 'ls -l':
-        ls_l()
+        list_directory.ls_l()
 
     elif command == 'ls -la':
-        ls_la()
+        list_directory.ls_la()
 
     else:
         print('Command not recognised, did you mean \'ls\'?')
 
 
-def ls():
-    """ Unix most basic implementation of the 'ls' command. """
-    l = [print(i) for i in os.listdir()]
-    return l
+class ListStructure():
 
+    def ls(self):
+        """ Unix most basic implementation of the 'ls' command. """
+        list_directory = [i for i in os.listdir() if
+                          not i.startswith('.')]
+        # print(list_directory)
 
-def ls_l():
-    """ Outputs 'ls -l' unix tool. """
-    ls = [i for i in os.listdir()]
-    for i in ls:
-        permissions = (oct(os.lstat(i).st_mode))[-4:]
-        size = os.path.getsize(i)
-        time = convert_time(i)
-        uid = os.getuid()
-        # pwd.getpwuid(os.stat(i).st_uid).pw_name
-        # this will return user name rather than number.
-        gid = os.getgid()
-        print(f'{permissions}\t{uid} {gid}\t{size}B\t{time}\t {i}')
+        return list_directory
 
-    pass
+    def ls_l(self):
+        """ Outputs 'ls -l' unix tool. """
+        info = dict()
 
+        ls = self.ls()
 
-def ls_la():
-    pass
+        for i in ls:
+            info['permissions'] = (oct(os.lstat(i).st_mode))[-4:]
+            info['size'] =  os.path.getsize(i)
+            info['time'] = ListStructure.convert_time(i) # This doesn't look very pythonic...
+            # info['uid'] = os.getuid()
+            info['uid'] = pwd.getpwuid(os.stat(i).st_uid).pw_name
+            # pwd.getpwuid(os.stat(i).st_uid).pw_name
+            # this will return user name rather than number.
+            info['gid'] = os.getgid()
+            print(f"{info['permissions']}\t{info['uid']} {info['gid']}\t"
+                  f"{info['size']}B\t{info['time']}\t {i}")
 
+    def ls_la(self):
+        pass
 
-def convert_time(file):
-    """ Helper function for time conversion. """
+    def convert_time(file):
+        """ Helper function for time conversion. """
 
-    t = os.path.getmtime(file)
-    utcts = datetime.datetime.utcfromtimestamp(t)
-    fmt = '%-d %b %H:%M'
+        t = os.path.getmtime(file)
+        utcts = datetime.datetime.utcfromtimestamp(t)
+        fmt = '%-d %b %H:%M'
 
-    return utcts.strftime(fmt)
-
-    # return datetime.datetime.fromtimestamp(t)
+        return utcts.strftime(fmt)
 
 
 def banner():
