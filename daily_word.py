e8712fc073da82b8ee59616abd3b75db1256919e
Dan <dans.address@outlook.com>
2018-02-11 16:54:46 +0000

    added function(etl).

--- daily_word.py
+++ daily_word.py
@@ -5,7 +5,8 @@ from pprint import pprint
 
 
 def main():
-    import_words()
+    file = import_words()
+    etl(file)
 
 
 def import_words():
@@ -14,29 +15,27 @@ def import_words():
     with open('words.txt', 'r') as fin:
         for line in fin.readlines():
             word_list.append(line)
+    return word_list
 
-    word_list = clean_text(word_list)
+def etl(word_list):
+    """Take in the word list and remove redundancies and cleanup text."""
 
+    word_list = clean_text(word_list) # clean up the text
     word_list = [word for word in word_list if len(word) != 1]
+    rm_non_letters = [word[0] for word in word_list if not ]
+
+    # remove non-definition words e.g headings.
 
     for idx, words in enumerate(word_list):
-        if len(words) != 1:
-            print("[{}] - {} : length == {}".format(idx, words, len(words)))
+        print("[{}] - {} : length == {}".format(idx, words, len(words)))
 
-    words = [x[0] for x in word_list]
-    words_upper = [x.upper() for x in words]
 
 
 def clean_text(text: list):
-    caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     if not text:
         return text
     txt = [txt.strip('').strip('\n') for txt in text]  # rm '' & newlines
     txt = list(filter(None, txt))  # remove null strings
-    # txt = filter(len(1), txt)
-
-    # if txt[0]
-
     return txt
 
 
