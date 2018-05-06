73f9685f7af2b6e3c386d10d16d1ea63816bca58
Dan <dans.address@outlook.com>
2018-05-06 07:40:53 +0000

    Login to account setup
    notes:
    + environment variables for username and password needed

--- autotipper.py
+++ autotipper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import time
 
 from selenium import webdriver
@@ -7,26 +8,25 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
 
 def main():
-    test()
+    login()
+    tips()
 
-def test():
-    # driver.implicitly_wait(3)
-    # driver.maximize_window()
+def login():
     driver.get('https://www.sportsbet.com.au/')
-
     driver.find_element_by_xpath('//*[@id="fakeusername"]').click()
     time.sleep(1)
     username = driver.find_element_by_xpath('//*[@id="username"]')
-    # username = driver.find_element_by_id("username")
-    # username.clear()
-
-    username.send_keys("TEST")
-
+    username.send_keys(os.getenv('USERNAME'))
     driver.find_element_by_xpath('//*[@id="fakepassword"]').click()
     time.sleep(1)
     password = driver.find_element_by_xpath('//*[@id="password"]')
+    password.send_keys(os.getenv('PASSWORD'))
+    driver.find_element_by_css_selector('#login-btn-ctn > a:nth-child(1)').click()
 
-    password.send_keys('assword')
+def tips():
+    driver.get('https://www.sportsbet.com.au/tipping/')
+    # driver.find_element_by_xpath('//*[@id="sbm-page-wrapper"]/section/a[3]')
+    time.sleep(2)
     driver.quit()
 
 
