269fe117bf5f0e982d063e72e508a7c0de5e8d6a
danielmichaels <dans.address@outlook.com>
2018-06-10 10:52:37 +0800

    Now knows how many RFC's are online and uses that as the max range
    notes:
    - fixes:
       - try block for all functions within main()
       - logging says how many new files downloaded
       - bs4 gets the total number of RFC's, but this is slow so I need to
       fix this.

--- rfc.py
+++ rfc.py
@@ -12,21 +12,27 @@ from random import choice
 logging.basicConfig(level=logging.INFO)
 
 def main():
-    check_folder_exists()
 
     try:
+        total_rfc = get_rfc_total()
+        logging.info(f'Current total of published RFC\'s: {total_rfc}')
+        check_folder_exists()
         folder = os.path.join(pathlib.Path.home(), 'code/test/RFC')
         os.chdir(folder)
         logging.info(f'changed dir to: {folder}')
         start = time()
-        iterate_over_rfcs()
+        iterate_over_rfcs(total_rfc)
         end = time()
         logging.info(f'This took: {end - start} to run!')
 
     except OSError:
         raise
 
+    except KeyboardInterrupt:
+        print('User exited using CTRL-C')
+
     finally:
+        logging.info(f'Total number RFC text files: {len(os.listdir())}')
         cur_dir = pathlib.Path(__file__).parent
         logging.info(f'changed dir to: {cur_dir}')
 
@@ -43,19 +49,18 @@ def random_header():
                  'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
     return {'User-Agent': choice(desktop_agents), 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
 
-def iterate_over_rfcs():
+def iterate_over_rfcs(total_rfc):
     """Iterate over all possible RFC numbers on the IEEE webpage and then
     write them individually to files with a separate folder."""
     session = FuturesSession(max_workers=10)
 
-    for num in range(0000, 8500):
+    for num in range(1, total_rfc):
         url = f"https://www.rfc-editor.org/rfc/rfc{num}.txt"
         future = session.get(url, headers=random_header())
         resp = future.result()
         if resp.status_code == 200:
             text = resp.text
             check_exists(num, text)
-            # create_files(num, text)
 
         else:
             print(f"RFC {num:04d} DOES NOT EXIST")
@@ -83,7 +88,15 @@ def check_exists(num, text):
     if filename not in lst:
         create_files(num, text)
 
-
+def get_rfc_total():
+    url = 'https://www.rfc-editor.org/rfc-index.html'
+    resp = requests.get(url)
+    text = resp.text
+    soup = BeautifulSoup(text, 'html.parser')
+    tables = soup.find_all('table')
+    rfc_table = tables[2].find_all('tr')
+    print(len(tables))
+    return len(rfc_table)
 
 
 def check_folder_exists():
