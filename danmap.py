2b8b6e4b0094857674ad388edae66874c50ce9a2
Dan <dans.address@outlook.com>
2018-06-17 14:28:27 +0000

    Create Nmap similar output for connect() scan.

--- danmap.py
+++ danmap.py
@@ -1,28 +1,66 @@
 #!/usr/bin/env python
-
+import datetime
+import time
 import socket
+import sys
 
 
 def main():
-    tcp_scan()
+    cli()
 
 
-def tcp_scan():
-    host = 'localhost'
+def connect_scan(host, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(2)
 
-    for port in range(0, 1024):
+        status_code = sock.connect_ex((host, port))
+        if status_code == 0:
+            return status_code
+
+        sock.close()
+
+    except Exception:
+        pass
+
+
+def cli():
+    host = 'scanme.nmap.org'
+    host_ip = socket.gethostbyname(host)
+    closed_ports = []
+    open_ports = []
+
+    start = time.time()
+    for port in range(0, 104):
         try:
-            # print(f'Connecting to {host} on {port}')
-            sock = socket.socket()
-            sock.connect_ex((host, port))
-            banner = sock.recv(1024)
+            resp = connect_scan(host, port)
+            if resp == 0:
+                open_ports.append(port)
 
-            if banner:
-                banner = banner.decode('utf-8')
-                print(f"[*] {host}:{port} {banner}")
-            sock.close()
-        except:
-            pass
+            if resp == 1:
+                closed_ports.append(port)
+
+        except KeyboardInterrupt:
+            print("[!!] Application Stopping...")
+            print("[!!] Keyboard Interrupt!")
+            sys.exit(1)
+    stop = time.time()
+    total_time = stop - start
+    printer(host, host_ip, closed_ports, open_ports, total_time)
+
+
+def printer(host, host_ip, closed_ports, open_ports, total_time):
+    print(f'Starting DaNmap 1.0 at {datetime.datetime.now()}')
+    print(f'DaNmap report for {host} ({host_ip})')
+    print(f'Not shown: {len(closed_ports)} Ports Closed')
+    print()
+
+    print('PORT\tSTATE\tSERVICE')
+    for port in open_ports:
+        print(f'{port}')
+
+    print()
+    print(f"Scan completed in {total_time} seconds")
 
 
 if __name__ == '__main__':
