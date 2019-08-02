08f994cf62a710116a8af06e922a99020b1b4643
danielmichaels <dans.address@outlook.com>
2019-08-02 11:18:20 +1000

    improve fizzbuzz with enumerate

--- scripts/fizzbuzz.py
+++ scripts/fizzbuzz.py
@@ -1,16 +1,41 @@
 #!python3
+"""
+FizzBuzz
 
-# is multiple of 3 return 'Fizz'
-# is multiple of 5 return 'Buzz'
-# is multiple of both return 'FizzBuzz'
+Rules:
 
-for i in range(101):
-    if i % 3 == 0 and i % 5 ==0:
-        print(f'{i}: FizzBuzz')
-    elif i % 3 == 0:
-        print(f'{i}: Fizz')
-    elif i % 5 == 0:
-        print(f'{i}: Buzz')
-    else:
-        print(f'{i}: ')
+1. multiples of 3 return 'Fizz'
+2. multiples of 5 return 'Buzz'
+3. multiples of both return 'FizzBuzz'
 
+"""
+
+def main():
+    plain_range()
+    enum_range()
+
+def plain_range():
+    for i in range(101):
+        if i % 3 == 0 and i % 5 == 0:
+            print(f'{i}: FizzBuzz')
+        elif i % 3 == 0:
+            print(f'{i}: Fizz')
+        elif i % 5 == 0:
+            print(f'{i}: Buzz')
+        else:
+            print(f'{i}: ')
+
+
+def enum_range():
+    for k, v in enumerate(range(0, 101)):
+        if k % 3 == 0 and k % 5 == 0:
+            print(k, "FizzBuzz")
+        elif k % 3 == 0:
+            print(k, "Fizz")
+        elif k % 5 == 0:
+            print(k, "Buzz")
+        else:
+            continue
+
+if __name__ == '__main__':
+    main()
