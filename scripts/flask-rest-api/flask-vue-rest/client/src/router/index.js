d79bbf7221c9cc3a5eec50c84f36e84c502c3eb2
danielmichaels <dans.address@outlook.com>
2019-11-13 13:51:58 +1100

    Add flask and vue rest api for reference

---
+++ scripts/flask-rest-api/flask-vue-rest/client/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Books from '../components/Books.vue';
+import Ping from '../components/Ping.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/ping',
+      name: 'Ping',
+      component: Ping,
+    },
+    {
+      path: '/',
+      name: 'Books',
+      component: Books,
+    },
+  ],
+});
